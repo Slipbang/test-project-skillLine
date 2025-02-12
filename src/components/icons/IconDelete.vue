adf0ba14519536662dfc9b3d295ad935d4ecfc65
Dmitry Kiselman <kiseldimon1@gmail.com>
2025-02-12 13:38:44 +0200

    icons refactoring

---
+++ src/components/icons/IconDelete.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <svg width="10" height="3" viewBox="0 0 10 3" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M9.58329 1.41666H0.416626" stroke="#F1F4FD" stroke-width="1.5" stroke-miterlimit="10"/>
+  </svg>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/icons/SearchIcon.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M16.309 16.5L21 21M19 10.0476C19 15.0445 14.9706 19.0952 10 19.0952C5.02944 19.0952 1 15.0445 1 10.0476C1 5.05076 5.02944 1 10 1C14.9706 1 19 5.05076 19 10.0476Z" stroke="#0E0E10" stroke-width="1.5" stroke-linecap="square" stroke-linejoin="round"/>
+  </svg>
+</template>
+
+<style scoped>
+
+</style>
