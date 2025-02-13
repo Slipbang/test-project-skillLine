82b944678e6dae6cbfd83bdf11d83548cccc91c4
Dmitry Kiselman <kiseldimon1@gmail.com>
2025-02-13 15:19:26 +0200

    code clearance

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import {createApp, type Directive} from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
-const app = createApp(App)
+const app = createApp(App);
 
 app.use(createPinia())
 
-app.mount('#app')
+app.mount('#app');
