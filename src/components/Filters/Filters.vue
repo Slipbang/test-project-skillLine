1e3b6dae58c4243178596e82529f358a5917c22d
Dmitry Kiselman <kiseldimon1@gmail.com>
2025-02-14 01:01:01 +0200

    added date filter scripts

--- src/components/Filters/Filters.vue
+++ src/components/Filters/Filters.vue
@@ -1,20 +1,28 @@
 <script setup lang="ts">
-import Filter from "@/components/Filter/Filter.vue";
+import Filter from "@/components/Filters/Filter/Filter.vue";
 import ArrowDownIcon from "@/components/icons/ArrowDownIcon.vue";
 import CalendarIcon from "@/components/icons/CalendarIcon.vue";
 import {useCalendarStore} from "@/stores/calendar.ts";
 
 import {onClickOutside} from "@vueuse/core";
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 import {storeToRefs} from "pinia";
+import {useFiltersStore} from "@/stores/filterStore.ts";
+import {defineDateText} from "@/utils/utilsFunction.ts";
 
 const divRef = ref<HTMLElement | null>(null);
 
 const calendarStore = useCalendarStore();
-const {isCalendarShown} = storeToRefs(calendarStore);
+const {isCalendarShown, dateTo, dateFrom} = storeToRefs(calendarStore);
+const filterStore = useFiltersStore();
+const {filterDateFrom, filterDateTo} = storeToRefs(filterStore);
+
+onMounted(() => {
+  filterStore.setDateTo(dateTo.value);
+  filterStore.setDateFrom(dateFrom.value);
+})
 
 onClickOutside(divRef,(event) => {
-
   const exceptions = [".custom-calendar"];
   const clickedInsideException = exceptions.some(selector =>
       (event.target as HTMLElement).closest(selector)
@@ -32,7 +40,7 @@ onClickOutside(divRef,(event) => {
     <Filter
         ref="divRef"
         @click="calendarStore.toggleCalendarVisibility()"
-        text="09 января 2024 - 15 января 2024"
+        :text="`${defineDateText(filterDateFrom)} - ${defineDateText(filterDateTo)}`"
         :icon="CalendarIcon"
     />
     <Filter
