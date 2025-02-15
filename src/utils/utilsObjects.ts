9e72517245ef12fb0f15b1131876e2ec0f2ac1f6
Dmitry Kiselman <kiseldimon1@gmail.com>
2025-02-15 03:45:25 +0200

    enum edded

--- src/utils/utilsObjects.ts
+++ src/utils/utilsObjects.ts
@@ -1,5 +1,6 @@
 export const days = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"];
 export const months = ['Январь', "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+export const filterMonths = ['Января', "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июль", "Августа", "Сентября", "Октября", "Ноября", "Декабря"];
 
 export const jsonData = [
     { name: "Иван", age: 25, city: "Москва" },
@@ -12,3 +13,11 @@ export const headersMap = {
     age: "Возраст",
     city: "Город"
 };
+
+export enum comparisonParams {
+    GT='GREATER THAN',
+    LT='LESS THAN',
+    GE="GREATER OR EQUAL",
+    LE='LESS OR EQUAL',
+    EQUAL='EQUAL'
+}
