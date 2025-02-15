b476863f423c124cc9ed2cf4d0dfc6fce4118f49
Dmitry Kiselman <kiseldimon1@gmail.com>
2025-02-15 03:44:27 +0200

    added filter and sort logic through pipeline

--- src/components/Table/Table.vue
+++ src/components/Table/Table.vue
@@ -1,8 +1,57 @@
 <script setup lang="ts">
-import SortIcon from "@/components/icons/SortIcon.vue";
 import CustomCheckbox from "@/components/CustomCheckbox/CustomCheckbox.vue";
-import {mockData} from "@/mockData.ts";
 import {defineEducationCategories} from "@/utils/utilsFunction.ts";
+import {useSchoolsApiStore} from "@/stores/schoolsApi.ts";
+import {onMounted, watch} from "vue";
+import {storeToRefs} from "pinia";
+import {useDebounceFn} from "@vueuse/core";
+import TableSortButton from "@/components/TableSortButton/TableSortButton.vue";
+import {useSortingStore} from "@/stores/sortStore.ts";
+import {sortData} from "@/utilsClasses/utilsSortFunctions.ts";
+import {filterByNames} from "@/utilsClasses/utilsFilterFunctions.ts";
+import {useFiltersStore} from "@/stores/filterStore.ts";
+
+const schoolsApiStore = useSchoolsApiStore();
+const {page, count} = storeToRefs(schoolsApiStore);
+
+const sortingStore = useSortingStore();
+const {
+  regionIsAscending,
+  nameIsAscending,
+  addressIsAscending,
+  educationLevelIsAscending,
+} = storeToRefs(sortingStore);
+
+const filterStore = useFiltersStore();
+const {filterInputValue} = storeToRefs(filterStore);
+
+const fetchDataDebounced = useDebounceFn((page: number, count: number) => {
+  schoolsApiStore.fetchData({
+    page,
+    count
+  });
+}, 500);
+
+onMounted(() => {
+  schoolsApiStore.fetchData({
+    page: page.value,
+    count: count.value
+  })
+})
+
+const {schoolItems} = storeToRefs(schoolsApiStore);
+
+
+const pipe =
+    <T>(...fns: ((args: Partial<T>) => Partial<T>)[]) =>
+        (initialArgs: Partial<T>): T =>
+            fns.reduce((acc, fn) => ({ ...acc, ...fn(acc) }), initialArgs) as T;
+
+watch([page, count], ([newPage, newCount]) => {
+  if (!newPage && !newCount) return;
+
+  fetchDataDebounced(newPage, newCount);
+})
 
 </script>
 
@@ -13,46 +62,80 @@ import {defineEducationCategories} from "@/utils/utilsFunction.ts";
       <th>
         <div>
           <div>
-            <CustomCheckbox fillColor="#1B1B1F" :checked="false" />
+            <CustomCheckbox fillColor="#1B1B1F" :checked="false"/>
             <p>Регион</p>
           </div>
-          <SortIcon/>
+
+          <TableSortButton
+              title="Сортировка: отключена | по возрастанию | по убыванию"
+              :isAscending="regionIsAscending"
+              @click="() => sortingStore.toggleRegionAscending()"
+          />
         </div>
       </th>
       <th>
         <div>
           <p>Название</p>
-          <SortIcon/>
+
+          <TableSortButton
+              title="Сортировка: отключена | по возрастанию | по убыванию"
+              :isAscending="nameIsAscending"
+              @click="sortingStore.toggleNameAscending()"
+          />
         </div>
       </th>
       <th>
         <div>
           <p>Адрес</p>
-          <SortIcon/>
+
+          <TableSortButton
+              title="Сортировка: отключена | по возрастанию | по убыванию"
+              :isAscending="addressIsAscending"
+              @click="sortingStore.toggleAddressAscending()"
+          />
         </div>
       </th>
       <th>
         <div>
           <p>Уровень образования</p>
-          <SortIcon/>
+
+          <TableSortButton
+              title="Сортировка: отключена | по возрастанию | по убыванию"
+              :isAscending="educationLevelIsAscending"
+              @click="sortingStore.toggleEducationLevelAscending()"
+          />
         </div>
       </th>
     </tr>
     </thead>
     <tbody>
-    <tr v-for="item in mockData" :key="item.uuid">
+    <tr v-for="item in pipe(
+        sortData,
+        filterByNames,
+    )({
+    schoolItems: schoolItems,
+     flags: {
+      eduLvlIsAsc: educationLevelIsAscending,
+      addressIsAsc: addressIsAscending,
+      nameIsAsc: nameIsAscending,
+      regionIsAsc: regionIsAscending
+    },
+    filterValue: filterInputValue
+
+    }).schoolItems"
+        :key="item.uuid">
       <td>
         <div class="region-container">
-          <CustomCheckbox fillColor="#1B1B1F" :checked="true" />
-          {{ item.region.name }}
+          <CustomCheckbox fillColor="#1B1B1F" :checked="true"/>
+          {{ item.edu_org.region.name }}
         </div>
       </td>
       <td>{{ item.edu_org.short_name ?? item.edu_org.full_name }}</td>
       <td>{{ item.edu_org.contact_info.post_address }}</td>
       <td>
         <div class="key-container">
-          <div class="category" v-for="keyWord in defineEducationCategories(item.supplements)" :key="keyWord">
-            <p>{{keyWord}}</p>
+          <div class="category" v-for="keyWord in defineEducationCategories(item.supplements)" key="keyWord">
+            <p>{{ keyWord }}</p>
           </div>
         </div>
       </td>
@@ -79,6 +162,7 @@ import {defineEducationCategories} from "@/utils/utilsFunction.ts";
 
     div {
       display: flex;
+      align-items: center;
       justify-content: space-between;
     }
   }
@@ -87,6 +171,7 @@ import {defineEducationCategories} from "@/utils/utilsFunction.ts";
     transition: background-color 0.3s ease-out;
     background-color: transparent;
   }
+
   tr:hover {
     background-color: #F1F4FD;
   }
@@ -126,9 +211,11 @@ import {defineEducationCategories} from "@/utils/utilsFunction.ts";
     }
 
   }
+
   td:last-child {
     width: 265px;
   }
+
   td:not(:last-child) {
     width: 26.00%;
   }
