2c519d0813a23fc4855d9934968db20cad16ab00
Dmitry Kiselman <kiseldimon1@gmail.com>
2025-02-15 03:40:24 +0200

    added states

--- src/stores/filterStore.ts
+++ src/stores/filterStore.ts
@@ -15,10 +15,21 @@ export const useFiltersStore = defineStore('filters', () => {
         year: null,
     });
 
+    const educationTypes = ref<string>('');
+    const institutionStatus = ref<string>('');
+
+    const filterInputValue = ref<string>('');
+
+    const setEducationType = (type: string) => {
+        educationTypes.value = type;
+    }
+    const setInstitutionStatus = (status: string) => {
+        institutionStatus.value = status;
+    }
+
     const setDateFrom = (date: IDate) => {
         filterDateFrom.value = date;
     }
-
     const setDateTo = (date: IDate) => {
         filterDateTo.value = date;
     }
@@ -26,6 +37,11 @@ export const useFiltersStore = defineStore('filters', () => {
     return {
         filterDateFrom,
         filterDateTo,
+        educationTypes,
+        institutionStatus,
+        filterInputValue,
+        setEducationType,
+        setInstitutionStatus,
         setDateFrom,
         setDateTo
     }
