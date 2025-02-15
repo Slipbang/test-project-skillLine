5e5fef7bf8bd292db33ac374b8d66ff62ac6e603
Dmitry Kiselman <kiseldimon1@gmail.com>
2025-02-15 03:46:08 +0200

    added api for backend

---
+++ src/stores/schoolsApi.ts
@@ -0,0 +1,61 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+import type {ISchoolsItem, serverResponse} from "@/types/types.ts";
+
+interface IInitialState {
+    schoolItems: ISchoolsItem[];
+    pages_count: number;
+    count: number;
+    page: number;
+    loading: boolean;
+    error: string | null;
+}
+
+interface IFetchDataParams {
+    page: number,
+    count: number,
+    federal_district_id?: number,
+    region_id?: number,
+    updated_at?: number
+}
+
+export const useSchoolsApiStore = defineStore("data", {
+    state: (): IInitialState => ({
+        schoolItems: [],
+        pages_count: 10,
+        count: 10,
+        page: 1,
+        loading: false,
+        error: null,
+    }),
+    actions: {
+        async fetchData({page, count, federal_district_id, region_id, updated_at}: IFetchDataParams): Promise<void> {
+            this.loading = true;
+            this.error = null;
+
+            const queryParams = new URLSearchParams({
+                page: page.toString(),
+                count: count.toString(),
+            });
+            if (federal_district_id) queryParams.append("federal_district_id", federal_district_id.toString());
+            if (region_id) queryParams.append("region_id", region_id.toString());
+            if (updated_at) queryParams.append("updated_at", updated_at.toString());
+
+            try {
+                const response = await axios.get<serverResponse>(`https://schooldb.skillline.ru/api/schools?${queryParams}`);
+                this.schoolItems = response.data.data.list;
+                this.pages_count = response.data.data.pages_count;
+            } catch (err) {
+                this.error = err instanceof Error ? err.message : "Ошибка запроса";
+            } finally {
+                this.loading = false;
+            }
+        },
+        selectPage(page: number) {
+            this.page = page;
+        },
+        selectCount(count: number) {
+            this.count = count;
+        },
+    },
+});
