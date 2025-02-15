69d7768afc56b4b18b2a8e97d5041319f09c0440
Dmitry Kiselman <kiseldimon1@gmail.com>
2025-02-15 03:43:54 +0200

    added prop

--- src/components/icons/SortIcon.vue
+++ src/components/icons/SortIcon.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-
+defineProps({
+  strokeDown: String,
+})
 </script>
 
 <template>
   <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M12.3333 6L8.32972 2L4.33325 6" stroke="#93939B" stroke-width="1.5" stroke-miterlimit="10"/>
-    <path d="M4.33325 10L8.33679 14L12.3333 10" stroke="#0E0E10" stroke-width="1.5" stroke-miterlimit="10"/>
+    <path d="M4.33325 10L8.33679 14L12.3333 10" :stroke="strokeDown" stroke-width="1.5" stroke-miterlimit="10"/>
   </svg>
 
 </template>
@@ -13,3 +15,5 @@
 <style scoped>
 
 </style>
+
+//#0E0E10
