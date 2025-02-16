cdd24b4e1c3f0275115d37133014aa0b6023d476
Dmitry Kiselman <kiseldimon1@gmail.com>
2025-02-16 04:45:58 +0200

    file deleted

--- src/utilsClasses/utilsFilterFunctions.ts
+++
@@ -1,8 +0,0 @@
-import type {PipeParams} from "@/types/types.ts";
-
-export const filterByNames = ({schoolItems, filterValue}: Partial<PipeParams>): Partial<PipeParams> => {
-    return {
-        schoolItems: schoolItems!.filter(item => (item.edu_org.short_name ?? item.edu_org.full_name).toLowerCase().includes(filterValue!.toLowerCase())),
-        filterValue,
-    };
-}
