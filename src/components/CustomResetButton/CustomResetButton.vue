a2a6111020b9618c08a5eb0ca8c7b576fbda91fc
Dmitry Kiselman <kiseldimon1@gmail.com>
2025-02-17 01:58:57 +0200

    added custom reset button

---
+++ src/components/CustomResetButton/CustomResetButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+import ResetIcon from "@/components/icons/ResetIcon.vue";
+</script>
+
+<template>
+  <button class="reset-button">
+    <ResetIcon />
+  </button>
+</template>
+
+<style scoped lang="scss">
+@use "@/assets/mixins";
+
+.reset-button {
+  @include mixins.round-button;
+}
+</style>
