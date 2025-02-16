c194facb3579a690c4fa1d7de63342fa29181e96
Dmitry Kiselman <kiseldimon1@gmail.com>
2025-02-16 04:40:26 +0200

    added components with filter logic

---
+++ src/components/Filters/FilterDropdown/FilterDropdown.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import FilterItem from "@/components/Filters/FilterDropdown/FilterItem/FilterItem.vue";
+import type {TSelectItem} from "@/types/types.ts";
+interface Props {
+  mapItems?: TSelectItem[],
+  isOpened?: boolean,
+  selectHandler?: ({name, id}: TSelectItem) => void,
+}
+defineProps<Props>()
+
+</script>
+
+<template>
+  <div :class="isOpened ? 'filter-list-open' : 'filter-list'">
+    <FilterItem
+        v-for="item in mapItems"
+        :text="item.name!"
+        :key="item.id"
+        :isOpened="isOpened"
+        @click="!!selectHandler && selectHandler({name:item.name, id: item.id})"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.filter-list {
+  text-align: center;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  transition: width 0.3s ease-out;
+  border-radius: 8px;
+
+  &-open {
+    @extend .filter-list;
+    padding: 5px;
+  }
+
+  .filter-wrapper:has(input) &-open {
+    @extend .filter-list;
+    margin-top: 56px;
+  }
+}
+</style>
---
+++ src/components/Filters/FilterDropdown/FilterItem/FilterItem.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+interface Props {
+  text: string,
+  isOpened?: boolean,
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div :class="isOpened ? 'filter-item-open' : 'filter-item'">
+    {{text}}
+  </div>
+</template>
+
+<style scoped lang="scss">
+.filter-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #E4E4EE;
+  width: 100%;
+  height: 0;
+  padding: 0 8px;
+  border-radius: 8px;
+  transition: all 0.3s ease-out;
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+  &-open {
+    @extend .filter-item;
+    margin: 1px 0;
+    height: 56px;
+    transition: all 0.3s ease-out;
+  }
+}
+</style>
