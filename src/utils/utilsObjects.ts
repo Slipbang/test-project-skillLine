d482119a96c63a2fe84f030db65f079d07294702
Dmitry Kiselman <kiseldimon1@gmail.com>
2025-02-16 04:46:31 +0200

    added some utility objects

--- src/utils/utilsObjects.ts
+++ src/utils/utilsObjects.ts
@@ -1,3 +1,5 @@
+import type {TSelectItem} from "@/types/types.ts";
+
 export const days = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"];
 export const months = ['Январь', "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
 export const filterMonths = ['Января', "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июль", "Августа", "Сентября", "Октября", "Ноября", "Декабря"];
@@ -21,3 +23,21 @@ export enum comparisonParams {
     LE='LESS OR EQUAL',
     EQUAL='EQUAL'
 }
+
+export const educationCategoriesNames: TSelectItem[] = [
+    {name: 'Все виды', id: 0},
+    {name: 'дошкольное', id: 1},
+    {name: 'общее', id: 2},
+    {name: 'среднее', id: 3},
+    {name: 'высшее', id: 4},
+    {name: 'бакалавриат', id: 5},
+    {name: 'специалитет', id: 6},
+    {name: 'магистратура', id: 7},
+    {name: 'послевузовское', id: 8},
+]
+
+export const statuses: TSelectItem[] = [
+    {name: 'Все статусы', id: 0},
+    {name: 'Недействующее', id: 1},
+    {name: 'Действующее', id: 2},
+]
