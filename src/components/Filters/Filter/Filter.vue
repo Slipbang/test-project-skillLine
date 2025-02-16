19d9119c89cb5c2662d6110c6a56f2e663f12933
Dmitry Kiselman <kiseldimon1@gmail.com>
2025-02-16 04:42:05 +0200

    some layout + logic fixes

--- src/components/Filters/Filter/Filter.vue
+++ src/components/Filters/Filter/Filter.vue
@@ -1,36 +1,71 @@
 <script setup lang="ts">
+import FilterDropdown from "@/components/Filters/FilterDropdown/FilterDropdown.vue";
+import type {TSelectItem} from "@/types/types.ts";
 
-defineProps({
-  text: String,
-  icon: Object,
-});
+interface Props {
+  text: string;
+  icon: Object;
+  mapItems?: TSelectItem[];
+  isOpened?: boolean;
+  selectHandler?: ({name, id}: TSelectItem) => void;
+}
+
+defineProps<Props>();
 </script>
 
 <template>
-  <div class="filter">
-    <p>{{text}}</p>
-    <component :is="icon" />
+  <div :class="isOpened ? 'filter-open' : 'filter'">
+    <slot />
+
+    <FilterDropdown
+        :mapItems="mapItems"
+        v-if="mapItems"
+        :isOpened="isOpened"
+        :selectHandler="selectHandler"
+    />
+    <div
+        class="filter-description"
+        v-if="!isOpened"
+    >
+      <p>{{text}}</p>
+      <component :is="icon" />
+    </div>
+
   </div>
 </template>
 
 <style scoped lang="scss">
 .filter {
-  height: 56px;
+  box-sizing: content-box;
+  width: 100%;
+  position: absolute;
   cursor: pointer;
-  flex: 1;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 20px;
+  max-height: 56px;
   border: 1px solid #D3D3DE;
   border-radius: 10px;
-  transition: background-color 0.3s ease-out;
+  transition: background-color 0.3s ease-out, all 0.3s ease-out;
+  z-index: 9997;
+  overflow: auto;
 
-  p {
-    font-size: 16px;
-    font-family: 'Gotham Pro', sans-serif;
-    font-weight: 400;
-    line-height: 21px;
+  .filter-description {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 20px;
+    height: 56px;
+
+    p {
+      font-size: 16px;
+      font-family: 'Gotham Pro', sans-serif;
+      font-weight: 400;
+      line-height: 21px;
+    }
+  }
+
+  &-open {
+    @extend .filter;
+    max-height: 600px;
+    transition: all 0.3s ease-out;
   }
 }
 
