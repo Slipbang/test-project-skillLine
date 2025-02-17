1624eb9c7d0498f91475db95d735e00434d00f47
Dmitry Kiselman <kiseldimon1@gmail.com>
2025-02-17 02:11:15 +0200

    added xlsx upload logic, input title, fixed layout

--- src/components/InstitutionsTable/InstitutionsTable.vue
+++ src/components/InstitutionsTable/InstitutionsTable.vue
@@ -2,9 +2,7 @@
 
 import Filters from "@/components/Filters/Filters.vue";
 import Table from "@/components/Table/Table.vue";
-//import {jsonToCSV} from "@/utils/utilsFunction.ts";
 import DataSelector from "@/components/DataSelector/DataSelector.vue";
-import PagesButtons from "@/components/Pagination/PagesButtons.vue";
 
 import CustomCalendar from "@/components/CustomCalendar/CustomCalendar.vue";
 import ButtonAccent from "@/components/ButtonAccent/ButtonAccent.vue";
@@ -13,19 +11,21 @@ import { storeToRefs } from "pinia";
 import {useCalendarStore} from "@/stores/calendar.ts";
 import {useFiltersStore} from "@/stores/filterStore.ts";
 import CustomInput from "@/components/CustomInput/CustomInput.vue";
+import {useXLSXStore} from "@/stores/xlsxStore.ts";
+import PaginationButtons from "@/components/Pagination/PaginationButtons.vue";
 
 const calendarStore = useCalendarStore();
 const {isCalendarShown} = storeToRefs(calendarStore);
 const filterStore = useFiltersStore();
 const {filterInputValue} = storeToRefs(filterStore);
 
-//jsonToCSV(jsonData, headersMap);
+const xlsxStore = useXLSXStore();
 
 </script>
 
 <template>
  <div class="stack">
-   <div  class="calendar">
+   <div class="calendar">
      <CustomCalendar
          v-if="isCalendarShown"
      />
@@ -38,7 +38,9 @@ const {filterInputValue} = storeToRefs(filterStore);
 
        <div class="controls">
          <CustomInput
+             text="Фильтрация учебных заведений по названию"
              v-model="filterInputValue"
+             @input="xlsxStore.resetSchoolItemsUuids()"
          />
          <ButtonAccent text="Скачать" />
        </div>
@@ -48,12 +50,12 @@ const {filterInputValue} = storeToRefs(filterStore);
        <Filters />
      </div>
 
-     <div class="table">
+     <div class="table-wrapper">
        <Table />
      </div>
 
      <div class="pagination">
-       <PagesButtons />
+       <PaginationButtons />
 
        <DataSelector />
      </div>
@@ -63,59 +65,88 @@ const {filterInputValue} = storeToRefs(filterStore);
 </template>
 
 <style scoped lang="scss">
+
+@use '@/assets/mixins';
 .stack {
   min-height: 932px;
   background-color: #F1F4FD;
   width: 100%;
   padding: 48px;
+}
+.calendar {
+  position: absolute;
+  top: 520px;
+  left: 56px;
+  z-index: 9999;
+}
 
-  .calendar {
-    position: absolute;
-    top: 520px;
-    left: 275px;
-    z-index: 9999;
+.block {
+  min-height: 836px;
+  border-radius: 16px;
+  background-color: #FFFFFF;
+  padding: 24px;
+}
+
+.cont {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  min-height: 56px;
+  align-items: center;
+  gap: 15px;
+  width: 100%;
+
+  h4 {
+    font-weight: 700;
+    font-size: 32px;
+    line-height:  38px;
+  }
+}
+.controls {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 20px;
+}
+
+.filters {
+  margin-top: 20px;
+}
+
+.table-wrapper {
+  margin-top: 20px;
+  height: 568px;
+  overflow: auto;
+  @include mixins.scroll-style;
+}
+
+.pagination {
+  margin-top: 25px;
+  gap: 15px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+@media (max-width: 992px) {
+  .cont {
+    justify-content: center;
+    h4 {
+      text-align: center;
+    }
+
   }
 
-  .block {
-    min-height: 836px;
-    border-radius: 16px;
-    background-color: #FFFFFF;
-    padding: 24px;
+  .controls {
+    width: 100%;
+    justify-content: space-between;
+  }
+}
 
-    .cont {
-      display: flex;
-      justify-content: space-between;
-      height: 56px;
-      align-items: center;
-
-      h4 {
-        font-weight: 700;
-        font-size: 32px;
-        line-height:  38px;
-      }
-
-      .controls {
-        display: flex;
-        align-items: center;
-
-        input {
-          margin-right: 24px;
-        }
-      }
-    }
-
-    .filters {
-      margin-top: 20px;
-    }
-
-    .table {
-      margin-top: 20px;
-    }
-
-    .pagination {
-      margin-top: 25px;
-      display: flex;
-      justify-content: space-between;
+@media (max-width: 630px) {
+  .controls {
+    input, button {
+      width: 100%;
     }
   }
 }
