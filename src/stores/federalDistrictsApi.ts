9ba82a601b81be27aa74f9858d0d6aa8c9652572
Dmitry Kiselman <kiseldimon1@gmail.com>
2025-02-17 13:25:03 +0200

    refactored error message

--- src/stores/federalDistrictsApi.ts
+++ src/stores/federalDistrictsApi.ts
@@ -19,6 +19,16 @@ export const useFederalDistrictsApiStore = defineStore("federalDistricts", {
             this.loading = true;
             this.error = null;
             this.federalDistricts = [];
+            const ssFederalDistricts = sessionStorage.getItem('federal-districts-api-store');
+            if (ssFederalDistricts && ssFederalDistricts.length > 0) {
+                try {
+                    this.federalDistricts = [...JSON.parse(ssFederalDistricts)];
+                    return;
+                } catch (err) {
+                    console.error(err)
+                }
+            }
+
             const tmpFedDis = {
                 id: -1,
                 name: 'Все Фед. округа',
@@ -32,8 +42,16 @@ export const useFederalDistrictsApiStore = defineStore("federalDistricts", {
                 const response = await axios.get<IServerResponse<IFederalDistrictsResponse>>(`https://schooldb.skillline.ru/api/federalDistricts`);
 
                 this.federalDistricts = [tmpFedDis,...response.data.data];
+                sessionStorage.setItem('federal-districts-api-store', JSON.stringify(this.federalDistricts));
             } catch (err) {
                 this.error = err instanceof Error ? err.message : "Ошибка запроса";
+                const errFedDis = {
+                    id: -1,
+                    name: 'Ошибка загрузки Фед. округа',
+                    short_name: '',
+                    code: '',
+                }
+                this.federalDistricts = [tmpFedDis, errFedDis];
             } finally {
                 this.loading = false;
             }
