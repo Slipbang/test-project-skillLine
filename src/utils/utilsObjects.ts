6c3f821f8fab31843bb02498bb45c3038d3d3863
Dmitry Kiselman <kiseldimon1@gmail.com>
2025-02-17 02:09:34 +0200

    mock data removed

--- src/utils/utilsObjects.ts
+++ src/utils/utilsObjects.ts
@@ -4,18 +4,6 @@ export const days = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"];
 export const months = ['Январь', "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
 export const filterMonths = ['Января', "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июль", "Августа", "Сентября", "Октября", "Ноября", "Декабря"];
 
-export const jsonData = [
-    { name: "Иван", age: 25, city: "Москва" },
-    { name: "Мария", age: 30 }, // Город отсутствует (ячейка будет пустая)
-    { name: "Олег", age: 28, city: "Новосибирск" }
-];
-
-export const headersMap = {
-    name: "Имя",
-    age: "Возраст",
-    city: "Город"
-};
-
 export enum comparisonParams {
     GT='GREATER THAN',
     LT='LESS THAN',
