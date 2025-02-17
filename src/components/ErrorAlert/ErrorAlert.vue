966478cf63118d81fe70effd1c2ed1f502eae609
Dmitry Kiselman <kiseldimon1@gmail.com>
2025-02-17 13:24:01 +0200

    added error alert component

---
+++ src/components/ErrorAlert/ErrorAlert.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+interface Props {
+  message: string;
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+<div class="error-alert">
+  <p>{{message}}</p>
+</div>
+</template>
+
+<style scoped lang="scss">
+.error-alert {
+  width: 100%;
+  height: calc(100% - 96px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+
+  p {
+    font-size: 36px;
+  }
+}
+</style>
